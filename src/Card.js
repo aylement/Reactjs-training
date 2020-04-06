eeee54e461ea022f0ddaf51cc13a22858ef306d2
aylement <33618420+aylement@users.noreply.github.com>
2020-04-06 13:02:36 +0200

    onto the proptypes!

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
@@ -11,5 +11,15 @@ const Card = ({ card, feedback, onClick }) => (
       </span>
     </div>
   )
-  
+  Card.propTypes = {
+    card: PropTypes.string.isRequired,
+    feedback: PropTypes.oneOf([
+        'hidden',
+        'justMatched',
+        'justMismatched',
+        'visible',
+    ]).isRequired,
+    onClick: PropTypes.func.isRequired,
+}
+
 export default Card
---
+++ src/HallOfFame.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import './HallOfFame.css'
+
+const HallOfFame = ({entries}) => (
+    <table className="hallOfFame">
+        <tbody>
+            {entries.map(({ id, date, guesses, player }) => (
+                <tr key={id}>
+                    <td className="date">{date}</td>
+                    <td className="guesses">{guesses}</td>
+                    <td className="player">{player}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+)
+
+export default HallOfFame
+
+// == Internal helpers ==============================================
+
+export const FAKE_HOF = [{
+        id: 3,
+        guesses: 18,
+        date: '10/10/2017',
+        player: 'One'
+    },
+    {
+        id: 2,
+        guesses: 23,
+        date: '11/10/2017',
+        player: 'Two'
+    },
+    {
+        id: 1,
+        guesses: 31,
+        date: '06/10/2017',
+        player: 'Three'
+    },
+    {
+        id: 0,
+        guesses: 48,
+        date: '14/10/2017',
+        player: 'Four'
+    },
+] 
